ea8338aba4157707a97f7fe809ea30fc48a75e93
Henrique Cardoso <henrique@oniweb.com.br>
2025-05-22 00:39:35 +0200

    Check env

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081';
-
 function App() {
+  let API_BASE_URL;
   if (process.env.NODE_ENV !== 'production') {
-    console.log('Environment:', process.env.NODE_ENV);
-    console.log('API_BASE_URL:', API_BASE_URL);
+    API_BASE_URL = 'http://localhost:8081';
   }else {
-    console.log('Environment: production');
+    API_BASE_URL = 'https://re-test-production.up.railway.app';
   }
+  
   console.log('API_BASE_URL:', API_BASE_URL);
   const [orderAmount, setOrderAmount] = useState('');
   const [result, setResult] = useState(null);
